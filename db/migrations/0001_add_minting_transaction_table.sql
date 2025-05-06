b66abef3ea3cc731c27153141a7c8a107e32cb49
rmeena840 <rmeena840@gmail.com>
2025-05-06 11:58:37 +0530

    feat: add minting transactions table
    Signed-off-by: rmeena840 <rmeena840@gmail.com>

---
+++ db/migrations/0001_add_minting_transaction_table.sql
@@ -0,0 +1,7 @@
+-- Migration number: 0001 	 2025-05-06T06:27:42.779Z
+CREATE TABLE minting_transactions (
+    txid TEXT PRIMARY KEY,
+    status TEXT NOT NULL CHECK(status IN ('pending', 'confirmed', 'failed'))
+);
+
+CREATE INDEX idx_txid ON minting_transactions(txid);
