af87e6a8e9324f99a5604eb8df4c9e1f3afedf0e
Ravindra Kumar Meena <rmeena840@gmail.com>
2025-05-07 18:18:07 +0530

    Update db/migrations/0001_add_minting_transaction_table.sql
    Co-authored-by: Robert Zaremba <robert@zaremba.ch>
    Signed-off-by: Ravindra Kumar Meena <rmeena840@gmail.com>

--- db/migrations/0001_add_minting_transaction_table.sql
+++ db/migrations/0001_add_minting_transaction_table.sql
@@ -1,5 +1,5 @@
 -- Migration number: 0001 	 2025-05-06T06:27:42.779Z
 CREATE TABLE minting_transactions (
     txid TEXT PRIMARY KEY,
-    status TEXT NOT NULL CHECK(status IN ('pending', 'confirmed', 'failed'))
+    status INTEGER NOT NULL CHECK(status IN (0,1,2,3))
 );
