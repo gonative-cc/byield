2452eb43fa87376e889c8771ba3967aa1feb44d4
rmeena840 <rmeena840@gmail.com>
2025-05-12 14:11:22 +0530

    lint fix
    Signed-off-by: rmeena840 <rmeena840@gmail.com>

--- app/components/OtcBuy.tsx
+++ app/components/OtcBuy.tsx
@@ -44,7 +44,7 @@ export function OtcBuy() {
 	const amountOfnBTC = numberOfSuiCoins / nBTCPrice;
 
 	useEffect(() => {
-		if (isSuiWalletConnected) {
+		if (isSuiWalletConnected) setValue("suiAddress", account?.address);
 	}, [account?.address, isSuiWalletConnected, setValue]);
 
 	return (
