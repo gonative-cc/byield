0a67d242b078ad080cd78e9b317d5a7720357dfb
rmeena840 <rmeena840@gmail.com>
2025-06-03 14:50:07 +0530

    feat: 404 page not found UI
    Signed-off-by: rmeena840 <rmeena840@gmail.com>

---
+++ app/routes/$.tsx
@@ -0,0 +1,17 @@
+import { Link } from "@remix-run/react";
+import { Button } from "~/components/ui/button";
+
+export default function PageNotFound() {
+	return (
+		<div className="flex h-[80vh] items-center justify-center">
+			<div className="flex flex-col items-center w-full gap-4 text-center">
+				<img src="/assets/404.svg" alt="Page not found" width={200} height={200} />
+				<span className="font-bold text-3xl">Error 404</span>
+				<span>Oops! Seems like you are lost :(</span>
+				<Link to="/">
+					<Button>Go back to main page</Button>
+				</Link>
+			</div>
+		</div>
+	);
+}
