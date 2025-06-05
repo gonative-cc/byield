9d9aa2e37ddaa8ede1b8ec5c65d38a59fe1be5fb
Ravindra Kumar Meena <rmeena840@gmail.com>
2025-06-05 18:59:30 +0530

    feat: hide bitcoin wallet on load (#85)
    * feat: hide bitcoin wallet on load
    
    Signed-off-by: rmeena840 <rmeena840@gmail.com>
    
    ---------
    
    Signed-off-by: rmeena840 <rmeena840@gmail.com>

--- app/providers/ByieldWalletProvider.tsx
+++ app/providers/ByieldWalletProvider.tsx
@@ -1,5 +1,5 @@
 import { useCurrentAccount } from "@mysten/dapp-kit";
-import { ReactNode, createContext, useEffect, useState } from "react";
+import { ReactNode, createContext, useEffect, useRef, useState } from "react";
 import { useXverseAddress } from "~/components/Wallet/XverseWallet/useXverseAddress";
 import { ByieldWallet } from "~/types";
 
@@ -9,34 +9,68 @@ interface WalletContextI {
 	isLoading: boolean;
 	connectedWallet: WalletType;
 	handleWalletConnect: (walletToBeConnected: WalletType) => void;
+	toggleBitcoinModal: (show: boolean) => void;
 }
 
 export const WalletContext = createContext<WalletContextI>({
 	isLoading: false,
 	connectedWallet: null,
 	handleWalletConnect: () => {},
+	toggleBitcoinModal: () => {},
 });
 
 export const ByieldWalletProvider = ({ children }: { children: ReactNode }) => {
 	const [isLoading, setIsLoading] = useState<boolean>(true);
 	const [connectedWallet, setConnectedWallet] = useState<WalletType>();
+	const [isModalHidden, setIsModalHidden] = useState<boolean>(true); // State to control modal visibility
 	const currentAccount = useCurrentAccount();
-	// check if SUI connection is active
-	const isSuiWalletActive = currentAccount !== null;
-	// check if bitcoin wallet is active
+	const isSuiWalletActive = !!currentAccount;
 	const { currentAddress } = useXverseAddress();
-	const isBitcoinWalletActive = currentAddress !== null;
+	const isBitcoinWalletActive = !!currentAddress;
+	const observerRef = useRef<MutationObserver | null>(null);
 
 	useEffect(() => {
 		setIsLoading(() => true);
+		// Update wallet state
 		setConnectedWallet(() =>
 			isSuiWalletActive ? ByieldWallet.SuiWallet : isBitcoinWalletActive ? ByieldWallet.Xverse : null,
 		);
 		setIsLoading(() => false);
-	}, [isBitcoinWalletActive, isSuiWalletActive]);
+	}, [isBitcoinWalletActive, isSuiWalletActive, isModalHidden]);
+
+	useEffect(() => {
+		const updateModalVisibility = () => {
+			const modal = document.getElementById("sats-connect-wallet-provider-selector");
+			if (modal) {
+				modal.style.display = isModalHidden ? "none" : "block";
+			}
+		};
+
+		// Run immediately to set initial modal state
+		updateModalVisibility();
+
+		const observer = new MutationObserver(() => {
+			updateModalVisibility();
+		});
+		observerRef.current = observer;
+		// Attach observer to the modal DOM node here if needed
+		observer.observe(document.body, { childList: true, subtree: true });
+
+		return () => {
+			observer.disconnect();
+		};
+	}, [isModalHidden]);
 
 	const handleWalletConnect = async (walletToBeConnected: WalletType): Promise<void> => {
 		setConnectedWallet(walletToBeConnected);
+		// hide the bitcoin wallet from document DOM
+		if (walletToBeConnected === ByieldWallet.Xverse) {
+			setIsModalHidden(false);
+		}
+	};
+
+	const toggleBitcoinModal = (show: boolean) => {
+		setIsModalHidden(!show);
 	};
 
 	return (
@@ -45,8 +79,16 @@ export const ByieldWalletProvider = ({ children }: { children: ReactNode }) => {
 				isLoading,
 				connectedWallet,
 				handleWalletConnect,
+				toggleBitcoinModal,
 			}}
 		>
+			<style>
+				{`
+          #sats-connect-wallet-provider-selector {
+            display: ${isModalHidden ? "none !important" : "block"};
+          }
+        `}
+			</style>
 			{children}
 		</WalletContext.Provider>
 	);
