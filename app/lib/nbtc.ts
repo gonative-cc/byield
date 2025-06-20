60f9f96f495983eadd0b7a72929944626fde9e37
Robert Zaremba <robert@zaremba.ch>
2025-06-20 10:43:17 +0200

    typo
    Signed-off-by: Robert Zaremba <robert@zaremba.ch>

--- app/lib/nbtc.ts
+++ app/lib/nbtc.ts
@@ -5,7 +5,7 @@ import type { UTXO, ValidateAddressI } from "~/api/btcrpc";
 import { ToastFunction } from "~/hooks/use-toast";
 
 export const nBTC_ADDR = "tb1qe60n447jylrxa96y6pfgy8pq6x9zafu09ky7cq";
-export const NBTC_COINT_TYPE =
+export const NBTC_COIN_TYPE =
 	"0x5419f6e223f18a9141e91a42286f2783eee27bf2667422c2100afc7b2296731b::nbtc::NBTC";
 
 export async function nBTCMintTxn(
