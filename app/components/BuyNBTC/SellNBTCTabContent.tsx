d3372fb31bdc245b36001f3947d222d81d4801e3
rmeena840 <rmeena840@gmail.com>
2025-06-20 19:15:03 +0530

    feat: make nBTC input dynamic
    Signed-off-by: rmeena840 <rmeena840@gmail.com>

--- app/components/BuyNBTC/SellNBTCTabContent.tsx
+++ app/components/BuyNBTC/SellNBTCTabContent.tsx
@@ -34,11 +34,11 @@ export function SellNBTCTabContent() {
 	});
 
 	const { watch, handleSubmit, reset } = sellNBTCForm;
-	const inputnBTCAmount = watch("nBTCAmount");
+	const inputNBTCAmount = watch("nBTCAmount");
 	const nBTCAmount = parseNBTC(
-		inputnBTCAmount?.length > 0 && inputnBTCAmount !== "." ? inputnBTCAmount : "0",
+		inputNBTCAmount?.length > 0 && inputNBTCAmount !== "." ? inputNBTCAmount : "0",
 	);
-	const SUI_AMOUNT_RECEIVED_ON_SELL = nBTCAmount * (PRICE_PER_NBTC_IN_SUI / 2n);
+	const SUIAmountReceived = nBTCAmount * (PRICE_PER_NBTC_IN_SUI / 2n);
 
 	const resetForm = useCallback(() => {
 		resetMutation();
@@ -47,7 +47,7 @@ export function SellNBTCTabContent() {
 		});
 	}, [reset, resetMutation]);
 
-	const suiAmountInputRules = {
+	const nBTCAmountInputRules = {
 		validate: {
 			isWalletConnected: () => isSuiWalletConnected || "Please connect SUI wallet",
 			enoughBalance: (value: string) => {
@@ -71,45 +71,43 @@ export function SellNBTCTabContent() {
 				}
 				className="flex w-full flex-col gap-2"
 			>
-				<div className="flex flex-col w-full gap-2">
-					<FormNumericInput
-						required
-						name="nBTCAmount"
-						placeholder="Enter nBTC amount"
-						className="h-16"
-						rightAdornments={<NBTCIcon className="mr-5" />}
-						rules={suiAmountInputRules}
-						createEmptySpace
-					/>
-					<span className="tracking-tighter text-gray-500 text-sm dark:text-gray-400">
-						This is a fixed price sell. The nBTC will be sold at price 12,500.
-					</span>
-					<ArrowDown className="text-primary justify-center w-full flex p-0 m-0" />
-					<FormNumericInput
-						name="SUIAmountReceived"
-						value={formatNBTC(SUI_AMOUNT_RECEIVED_ON_SELL)}
-						readOnly
-						className="h-16"
-						rightAdornments={<SUIIcon className="mr-2" />}
-					/>
-					{isSuiWalletConnected ? (
-						<Button type="submit" disabled={isPending} isLoading={isPending}>
-							Sell nBTC
-							<ChevronRight />
-						</Button>
-					) : (
-						<SuiModal />
-					)}
-					{(isSuccess || isError) && (
-						<Modal title={"Sell nBTC Transaction Status"} open handleClose={resetForm}>
-							<TransactionStatus
-								isSuccess={data?.effects?.status?.status === "success"}
-								handleRetry={resetForm}
-								txnId={data?.digest}
-							/>
-						</Modal>
-					)}
-				</div>
+				<FormNumericInput
+					required
+					name="nBTCAmount"
+					placeholder="Enter nBTC amount"
+					className="h-16"
+					rightAdornments={<NBTCIcon className="mr-5" />}
+					rules={nBTCAmountInputRules}
+					createEmptySpace
+				/>
+				<ArrowDown className="text-primary justify-center w-full flex p-0 m-0" />
+				<FormNumericInput
+					name="SUIAmountReceived"
+					value={formatNBTC(SUIAmountReceived)}
+					readOnly
+					className="h-16"
+					rightAdornments={<SUIIcon className="mr-2" />}
+				/>
+				<span className="tracking-tighter text-gray-500 text-sm dark:text-gray-400">
+					This is a fixed price sell. The nBTC will be sold at price 12,500.
+				</span>
+				{isSuiWalletConnected ? (
+					<Button type="submit" disabled={isPending} isLoading={isPending}>
+						Sell nBTC
+						<ChevronRight />
+					</Button>
+				) : (
+					<SuiModal />
+				)}
+				{(isSuccess || isError) && (
+					<Modal title={"Sell nBTC Transaction Status"} open handleClose={resetForm}>
+						<TransactionStatus
+							isSuccess={data?.effects?.status?.status === "success"}
+							handleRetry={resetForm}
+							txnId={data?.digest}
+						/>
+					</Modal>
+				)}
 			</form>
 		</FormProvider>
 	);
