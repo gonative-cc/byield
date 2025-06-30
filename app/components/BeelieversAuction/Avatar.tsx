cd64bdd31b03fb4d15913711854000b8c76974a0
rmeena840 <rmeena840@gmail.com>
2025-06-30 10:49:40 +0530

    PR comments
    Signed-off-by: rmeena840 <rmeena840@gmail.com>

--- app/components/BeelieversAuction/Avatar.tsx
+++ app/components/BeelieversAuction/Avatar.tsx
@@ -1,38 +1,9 @@
 import { Button } from "../ui/button";
 
-const AVATARS = [
-	{
-		key: "smith",
-		src: "/assets/avatar/smith.svg",
-	},
-	{
-		key: "alice",
-		src: "/assets/avatar/alice.svg",
-	},
-	{
-		key: "bruce",
-		src: "/assets/avatar/bruce.svg",
-	},
-	{
-		key: "julie",
-		src: "/assets/avatar/julie.svg",
-	},
-];
-
 export function Avatar() {
 	return (
 		<div className="flex flex-col gap-2 items-center">
 			<div className="flex flex-col md:flex-row gap-4 items-center">
-				<div className="flex -space-x-4 rtl:space-x-reverse">
-					{AVATARS.map(({ key, src }) => (
-						<img
-							key={key}
-							alt={key}
-							src={src}
-							className="w-10 h-10 rounded-full dark:border-gray-800"
-						/>
-					))}
-				</div>
 				<span>1349 whitelisted people put their bid for the auction</span>
 			</div>
 			<p className="text-gray-400">
