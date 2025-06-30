0747a1e322f0ebcc511d0a12819743623bdee71e
rmeena840 <rmeena840@gmail.com>
2025-06-30 14:37:21 +0530

    pr comments
    Signed-off-by: rmeena840 <rmeena840@gmail.com>

--- app/components/BeelieversAuction/CheckEligible.tsx
+++ app/components/BeelieversAuction/CheckEligible.tsx
@@ -11,66 +11,6 @@ interface CheckEligibleProps {
 	isEligible?: boolean;
 }
 
-function DesktopView() {
-	return (
-		<Card className="hidden md:block w-3/4">
-			<CardContent className="p-5 rounded-lg text-white flex gap-8 bg-azure-25">
-				<img src="/assets/bee/bee-with-hammer.svg" alt="bee-with-hammer" />
-				<div className="flex gap-2 py-4">
-					<div className="flex flex-col gap-2 justify-between">
-						<div className="flex justify-between">
-							<span className="text-2xl font-bold leading-10">
-								Participate in <br /> Beelievers Auction
-							</span>
-						</div>
-						<span className="text-sm">
-							You bid your true value; winners pay the lowest winning bid. Any amount above the
-							clearing price is refunded.
-						</span>
-						<Form method="POST" action="?check=eligibility">
-							<Button type="submit" className="flex w-[163px]">
-								<Wallet />
-								Check Eligibility
-							</Button>
-						</Form>
-					</div>
-					<div className="flex flex-col justify-between shrink-0 pt-2">
-						<AttemptAuction />
-						<span className="text-sm">Auction ends in 00 : 23 :12</span>
-					</div>
-				</div>
-			</CardContent>
-		</Card>
-	);
-}
-
-function MobileView() {
-	return (
-		<Card className="md:hidden block">
-			<CardContent className="p-4 rounded-lg text-white flex flex-col gap-8 bg-azure-25">
-				<img src="/assets/bee/bee-with-face-only.svg" alt="bee-with-hammer" />
-				<div className="flex w-full justify-between">
-					<span className="text-xl font-bold leading-8">Participate in Beelievers Auction</span>
-					<AttemptAuction className="self-start pt-2" />
-				</div>
-				<span className="text-sm self-start">
-					You bid your true value; winners pay the lowest winning bid. Any amount above the clearing
-					price is refunded.
-				</span>
-				<div className="flex gap-2 justify-between w-full items-end">
-					<Form method="POST" action="?check=eligibility">
-						<Button type="submit" className="flex w-[163px]">
-							<Wallet />
-							Check Eligibility
-						</Button>
-					</Form>
-					<span className="text-sm">Auction ends in 00 : 23 :12</span>
-				</div>
-			</CardContent>
-		</Card>
-	);
-}
-
 export function CheckEligible({ isEligible }: CheckEligibleProps) {
 	const shouldCheckEligibility = isEligible === undefined;
 	const isEligibleForAuction = !shouldCheckEligibility && isEligible;
@@ -78,8 +18,49 @@ export function CheckEligible({ isEligible }: CheckEligibleProps) {
 	if (shouldCheckEligibility) {
 		return (
 			<>
-				<DesktopView />
-				<MobileView />
+				<Card className="w-full md:w-[72%]">
+					<CardContent className="p-4 rounded-lg text-white flex flex-col md:flex-row gap-4 md:gap-8 bg-azure-25">
+						<img
+							src="/assets/bee/bee-with-hammer.svg"
+							alt="bee-with-hammer"
+							className="hidden md:block"
+						/>
+						<img
+							src="/assets/bee/bee-with-face-only.svg"
+							alt="bee-with-face-only"
+							className="md:hidden block"
+						/>
+						<div className="flex flex-col md:flex-row gap-2 md:gap-4 py-0 md:py-4 w-full">
+							<div className="flex flex-col gap-4 justify-between">
+								<div className="flex justify-between gap-2">
+									<span className="text-xl md:text-2xl font-bold leading-8 md:leading-10">
+										Participate in Beelievers Auction
+									</span>
+									<div className="flex flex-col justify-between shrink-0 md:hidden">
+										<AttemptAuction className="pt-2 self-end" />
+									</div>
+								</div>
+								<span className="text-sm">
+									You bid your true value; winners pay the lowest winning bid. Any amount above the
+									clearing price is refunded.
+								</span>
+								<div className="flex gap-2 justify-between w-full items-end">
+									<Form method="POST" action="?check=eligibility">
+										<Button type="submit" className="flex w-[163px]">
+											<Wallet />
+											Check Eligibility
+										</Button>
+									</Form>
+									<span className="text-sm md:hidden block">Auction ends in 00 : 23 :12</span>
+								</div>
+							</div>
+							<div className="md:flex flex-col justify-between shrink-0 hidden">
+								<AttemptAuction className="pt-2 self-start md:self-end" />
+								<span className="text-sm">Auction ends in 00 : 23 :12</span>
+							</div>
+						</div>
+					</CardContent>
+				</Card>
 				<Avatar />
 			</>
 		);
--- app/components/BeelieversAuction/NotEligible.tsx
+++ app/components/BeelieversAuction/NotEligible.tsx
@@ -3,24 +3,26 @@ import { Card, CardContent } from "../ui/card";
 import { Button } from "../ui/button";
 import { AttemptAuction } from "./AttemptAuction";
 
-function DesktopView() {
+export function NotEligible() {
 	return (
-		<Card className="hidden md:block w-[72%]">
-			<CardContent className="p-5 rounded-lg text-white flex gap-8 bg-azure-25">
-				<img src="/assets/bee/bee-looking-right.svg" alt="bee-with-hammer" />
-				<div className="flex gap-8 py-4">
-					<div className="flex flex-col gap-2 justify-between">
-						<div className="flex justify-between">
-							<span className="text-2xl font-bold leading-10">
-								Looks like you’re not on the list yet.
-								<br />
-								Good news? That can change
+		<Card className="w-full md:w-[72%]">
+			<CardContent className="p-4 rounded-lg text-white flex flex-col md:flex-row gap-4 md:gap-8 bg-azure-25">
+				<img src="/assets/bee/bee-looking-right.svg" alt="bee-with-hammer" className="hidden md:block" />
+				<img src="/assets/bee/bee-with-face-only.svg" alt="bee-with-hammer" className="md:hidden block" />
+				<div className="flex flex-col md:flex-row gap-2 md:gap-8 py-0 md:py-4 w-full">
+					<div className="flex flex-col gap-4 justify-between">
+						<div className="flex justify-between gap-2">
+							<span className="text-xl md:text-2xl font-bold leading-8 md:leading-10">
+								Looks like you’re not on the list yet. Good news? That can change
 							</span>
+							<div className="flex flex-col justify-between shrink-0 md:hidden">
+								<AttemptAuction className="pt-2 self-start md:self-auto" />
+							</div>
 						</div>
 						<span className="text-sm">
 							Head over to{" "}
 							<Link to="/">
-								<Button variant="link" className="p-0">
+								<Button variant="link" className="p-0 m-0">
 									BYield
 								</Button>
 							</Link>{" "}
@@ -28,46 +30,11 @@ function DesktopView() {
 						</span>
 						<span className="text-sm">Auction ends in 00 : 23 :12</span>
 					</div>
-					<div className="flex flex-col justify-between shrink-0">
-						<AttemptAuction className="pt-2" />
+					<div className="md:flex flex-col justify-between shrink-0 hidden">
+						<AttemptAuction className="pt-2 self-start md:self-auto" />
 					</div>
 				</div>
 			</CardContent>
 		</Card>
-	);
-}
-
-function MobileView() {
-	return (
-		<Card className="md:hidden block">
-			<CardContent className="p-5 rounded-lg text-white flex flex-col gap-4 bg-azure-25">
-				<img src="/assets/bee/bee-with-face-only.svg" alt="bee-with-hammer" />
-				<div className="flex gap-2 justify-between">
-					<span className="text-xl font-bold leading-8 w-3/4">
-						Looks like you’re not on the list yet. Good news? That can change
-					</span>
-					<AttemptAuction className="self-start align-end pt-2 w-fit" />
-				</div>
-				<span className="text-sm">
-					Head over to{" "}
-					<Link to="/">
-						<Button variant="link" className="p-0 m-0">
-							BYield
-						</Button>
-					</Link>{" "}
-					and perform testnet transaction to access whitelist form.
-				</span>
-				<span className="text-sm">Auction ends in 00 : 23 :12</span>
-			</CardContent>
-		</Card>
-	);
-}
-
-export function NotEligible() {
-	return (
-		<>
-			<DesktopView />
-			<MobileView />
-		</>
 	);
 }
