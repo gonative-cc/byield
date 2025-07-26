c252c53d00f84869623e1613344cd701cb45d21f
Robert Zaremba <robert@zaremba.ch>
2025-07-26 22:26:44 +0300

    lint: rework the config and ignore workers/app.ts
    Signed-off-by: Robert Zaremba <robert@zaremba.ch>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,144 +1,59 @@
-import { defineConfig } from "eslint/config";
 import globals from "globals";
-import {
-  fixupConfigRules,
-  fixupPluginRules,
-  includeIgnoreFile,
-} from "@eslint/compat";
-import react from "eslint-plugin-react";
-import jsxA11Y from "eslint-plugin-jsx-a11y";
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import _import from "eslint-plugin-import";
-import tsParser from "@typescript-eslint/parser";
+import tseslint from "typescript-eslint";
+import js from "@eslint/js";
+import { includeIgnoreFile } from "@eslint/compat";
+import { defineConfig } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+import pluginReact from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+
 import path from "node:path";
 import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
-import eslintConfigPrettier from "eslint-config-prettier";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
 const gitignorePath = path.resolve(__dirname, ".gitignore");
 
 export default defineConfig([
   includeIgnoreFile(gitignorePath),
   {
-    ignores: ["worker-configuration.d.ts"],
+    ignores: ["worker-configuration.d.ts", "workers/app.ts"],
   },
+
   {
-    extends: compat.extends("eslint:recommended"),
-
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.commonjs,
-      },
-
-      ecmaVersion: "latest",
-      sourceType: "module",
-
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true,
-        },
-      },
-    },
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
   },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  reactHooks.configs["recommended-latest"],
+  jsxA11y.flatConfigs.recommended,
+
   {
-    files: ["**/*.{js,jsx,ts,tsx}"],
-
-    extends: fixupConfigRules(
-      compat.extends(
-        "plugin:react/recommended",
-        "plugin:react/jsx-runtime",
-        "plugin:react-hooks/recommended",
-        "plugin:jsx-a11y/recommended",
-      ),
-    ),
-
-    plugins: {
-      react: fixupPluginRules(react),
-      "jsx-a11y": fixupPluginRules(jsxA11Y),
-    },
-
     settings: {
       react: {
-        version: "detect",
-      },
-
-      formComponents: ["Form"],
-
-      linkComponents: [
-        {
-          name: "Link",
-          linkAttribute: "to",
-        },
-        {
-          name: "NavLink",
-          linkAttribute: "to",
-        },
-      ],
-
-      "import/resolver": {
-        typescript: {},
+        version: "detect", // React version. "detect" automatically picks the version you have installed.
       },
     },
   },
+
   {
-    files: ["**/*.{ts,tsx}"],
-
-    extends: fixupConfigRules(
-      compat.extends(
-        "plugin:@typescript-eslint/recommended",
-        "plugin:import/recommended",
-        "plugin:import/typescript",
-      ),
-    ),
-
-    plugins: {
-      "@typescript-eslint": fixupPluginRules(typescriptEslint),
-      import: fixupPluginRules(_import),
+    files: ["**/*.{jsx,tsx}"],
+    rules: {
+      "react/react-in-jsx-scope": "off",
     },
+    languageOptions: { globals: globals.browser },
+  },
 
-    languageOptions: {
-      parser: tsParser,
-    },
-
-    settings: {
-      "import/internal-regex": "^~/",
-
-      "import/resolver": {
-        node: {
-          extensions: [".ts", ".tsx"],
-        },
-
-        typescript: {
-          alwaysTryTypes: true,
-        },
-      },
-    },
-
+  {
+    files: ["**/*.{jsx,tsx,js,ts}"],
     rules: {
       "@typescript-eslint/no-unused-vars": [
         "warn",
-        {
-          argsIgnorePattern: "^_",
-        },
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
       ],
-    },
-  },
-  {
-    files: ["**/.eslintrc.cjs"],
-
-    languageOptions: {
-      globals: {
-        ...globals.node,
-      },
     },
   },
 
