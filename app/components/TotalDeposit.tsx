056e4a41f34ef0b621f6b4ea57b5b7ca57eeb9f6
Ravindra Kumar Meena <rmeena840@gmail.com>
2025-07-30 13:00:49 +0530

    feat: revamp market screen (#167)
    * feat: revamp market screen
    
    Signed-off-by: rmeena840 <rmeena840@gmail.com>

--- app/components/TotalDeposit.tsx
+++ app/components/TotalDeposit.tsx
@@ -88,7 +88,7 @@ export const columns: Column<DApp>[] = [
 
 function DepositCard({ title, value, tooltip }: DepositData) {
 	return (
-		<Card className="flex flex-1">
+		<Card className="flex flex-1 max-w-1/4">
 			<CardContent className="p-6 rounded-lg text-white flex flex-col gap-2 bg-azure-10 w-full">
 				<div className="flex gap-2">
 					<span className="text-base font-medium">{title}</span>
@@ -136,7 +136,7 @@ const mockVaults: DApp[] = [
 		type: "DEX",
 		labels: ["Boosted", "Farming"],
 		apy: 11.71,
-		chain: "30.555$",
+		chain: "SUI",
 		logo: "/assets/ui-icons/market/desigVault.svg",
 	},
 	{
@@ -144,76 +144,16 @@ const mockVaults: DApp[] = [
 		type: "LENDING",
 		labels: ["Boosted", "Farming"],
 		apy: 5.71,
-		chain: "23.555$",
+		chain: "SUI",
 		logo: "/assets/ui-icons/market/bucketProtocol.svg",
-	},
-	{
-		name: "Navi Protocol Vault",
-		type: "LENDING",
-		labels: ["Boosted", "Farming"],
-		apy: 10.71,
-		chain: "12.215$",
-		logo: "/assets/ui-icons/market/naviProtocolValut.svg",
-	},
-	{
-		name: "Splash",
-		type: "LENDING",
-		labels: ["Boosted", "Farming"],
-		apy: 1.44,
-		chain: "20.085$",
-		logo: "/assets/ui-icons/market/splash.svg",
-	},
-	{
-		name: "Pyth Network",
-		type: "DEX",
-		labels: ["Boosted", "Farming"],
-		apy: 9.71,
-		chain: "27.715$",
-		logo: "/assets/ui-icons/market/pythNetwork.svg",
-	},
-	{
-		name: "Scallop",
-		type: "DEX",
-		labels: ["Boosted", "Farming"],
-		apy: 2.71,
-		chain: "29.885$",
-		logo: "/assets/ui-icons/market/scallop.svg",
-	},
-	{
-		name: "BlueMove",
-		type: "DEX",
-		labels: ["Boosted", "Farming"],
-		apy: 2.71,
-		chain: "11.235$",
-		logo: "/assets/ui-icons/market/blueMove.svg",
-	},
-	{
-		name: "OmniBTC",
-		type: "DEX",
-		labels: ["Boosted", "Farming"],
-		apy: 0.56,
-		chain: "00.805$",
-		logo: "/assets/ui-icons/market/omniBtc.svg",
 	},
 ];
 
 const MOCK_DEPOSIT_DATA: DepositData[] = [
 	{
-		id: "totalValue",
-		title: "Total Value Routed",
-		value: "$9.62M",
-		tooltip: "",
-	},
-	{
-		id: "totalDeposit",
-		title: "Total Deposit",
-		value: "$14.72M",
-		tooltip: "",
-	},
-	{
-		id: "totalBorrow",
-		title: "Total Borrow",
-		value: "$9.23M",
+		id: "nbtc-balance",
+		title: "nBTC Balance",
+		value: "12.34",
 		tooltip: "",
 	},
 ];
