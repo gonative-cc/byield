543d0ee21a0fcaf8ee1d95df7ba11b0f0477cdf4
rmeena840 <rmeena840@gmail.com>
2025-07-31 17:51:21 +0530

    feat: mint integration
    Signed-off-by: rmeena840 <rmeena840@gmail.com>

--- app/components/MintBTC.tsx
+++ app/components/MintBTC.tsx
@@ -10,7 +10,10 @@ import { WalletContext } from "~/providers/ByieldWalletProvider";
 import { Wallets } from "~/components/Wallet";
 import { FormNumericInput } from "./form/FormNumericInput";
 import { NumericFormat } from "react-number-format";
-import { formatBTC, parseBTC } from "~/lib/denoms";
+import { BTC, formatBTC, parseBTC } from "~/lib/denoms";
+import { nBTCMintTxn } from "~/lib/nbtc";
+import { networks, type Network } from "bitcoinjs-lib";
+import { useToast } from "~/hooks/use-toast";
 
 const nBTCMintFeeInSatoshi = 10n;
 
@@ -35,13 +38,13 @@ const PERCENTAGES = [
 
 function Percentage({ onChange }: { onChange: (value: number) => void }) {
 	return (
-		<div className="flex justify-between mb-4 space-x-2">
+		<div className="flex justify-between space-x-2 mb-4">
 			{PERCENTAGES.map(({ id, value }) => (
 				<Button
 					type="button"
 					key={id}
 					onClick={() => onChange(value)}
-					variant="outline"
+					variant="ghost"
 					className="bg-azure-10 w-full flex-1 flex"
 				>
 					{value}%
@@ -79,9 +82,10 @@ interface MintNBTCForm {
 }
 
 export function MintBTC() {
-	const { balance: walletBalance } = useXverseWallet();
+	const { balance: walletBalance, currentAddress } = useXverseWallet();
 	const { isWalletConnected } = useContext(WalletContext);
 	const isBitCoinWalletConnected = isWalletConnected(Wallets.Xverse);
+	const { toast } = useToast();
 	const balance = parseBTC(walletBalance ?? "0");
 	const mintNBTCForm = useForm<MintNBTCForm>({
 		mode: "all",
@@ -93,20 +97,27 @@ export function MintBTC() {
 	});
 
 	const { handleSubmit, watch, setValue } = mintNBTCForm;
-	const numberOfBTC = watch("numberOfBTC");
 
-	const youReceive = parseBTC(numberOfBTC || "0") - nBTCMintFeeInSatoshi;
+	const numberOfBTC = watch("numberOfBTC");
+	// const youReceive = parseBTC(numberOfBTC || "0") - nBTCMintFeeInSatoshi;
 
 	return (
 		<FormProvider {...mintNBTCForm}>
 			<form
-				onSubmit={handleSubmit((formData) => {
-					// TODO: handle the nbtc form data
-					console.log("Depositing with Sui Address:", formData);
+				onSubmit={handleSubmit(async ({ numberOfBTC, suiAddress }) => {
+					// TODO: Support for mainnet
+					if (currentAddress)
+						await nBTCMintTxn(
+							currentAddress,
+							Number(parseBTC(numberOfBTC)),
+							suiAddress,
+							networks.testnet,
+							toast,
+						);
 				})}
 			>
 				<Card>
-					<CardContent className="p-6 rounded-lg text-white flex flex-col gap-4 bg-azure-10">
+					<CardContent className="p-6 rounded-lg text-white flex flex-col bg-azure-10">
 						{isBitCoinWalletConnected && walletBalance && (
 							<BitcoinBalance availableBalance={walletBalance} />
 						)}
@@ -114,8 +125,11 @@ export function MintBTC() {
 							required
 							name="numberOfBTC"
 							placeholder="Enter number of BTC"
-							rightAdornments={<span className="text-sm font-medium w-20">~$0 USD</span>}
 							className="h-16"
+							inputMode="decimal"
+							decimalScale={BTC}
+							allowNegative={false}
+							createEmptySpace
 							rules={{
 								validate: {
 									isWalletConnected: () =>
@@ -127,8 +141,10 @@ export function MintBTC() {
 						/>
 						<Percentage
 							onChange={(value: number) => {
-								const val = (balance * BigInt(value)) / BigInt(100);
-								setValue("numberOfBTC", formatBTC(val));
+								if (walletBalance) {
+									const val = (BigInt(walletBalance) * BigInt(value)) / 100n;
+									setValue("numberOfBTC", formatBTC(BigInt(val)));
+								}
 							}}
 						/>
 						<FormInput
@@ -136,12 +152,14 @@ export function MintBTC() {
 							name="suiAddress"
 							placeholder="Enter destination Sui Address..."
 							className="h-16"
+							createEmptySpace
 						/>
-						{youReceive && (
+						{/* TODO: Add fee support in minting BTC */}
+						{/* {youReceive && (
 							<Fee feeInSatoshi={nBTCMintFeeInSatoshi} youReceive={formatBTC(youReceive)} />
-						)}
+						)} */}
 						<Button type="submit">Deposit BTC and mint nBTC</Button>
-						<div className="flex justify-between">
+						<div className="flex justify-between mt-2">
 							<span>TX ID: b99d9a361ac9db3...</span>
 							<Link
 								target="_blank"
--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,21 @@ import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 export default defineConfig({
 	plugins: [
-		cloudflare({ viteEnvironment: { name: "ssr" } }),
 		tailwindcss(),
 		reactRouter(),
 		tsconfigPaths(),
+		nodePolyfills({
+			globals: {
+				Buffer: true,
+				global: true,
+				process: true,
+			},
+		}),
+		cloudflare(),
 	],
 	build: {
 		minify: true,
