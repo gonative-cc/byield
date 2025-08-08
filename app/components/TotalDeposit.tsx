9cb8babdfa96309c82fd96f8ebd0510d45851115
Ravindra Kumar Meena <rmeena840@gmail.com>
2025-08-08 16:27:00 +0530

    feat: byield markets magma integration (#185)
    * feat: turbos integration
    
    Signed-off-by: rmeena840 <rmeena840@gmail.com>
    
    ---------
    
    Signed-off-by: rmeena840 <rmeena840@gmail.com>

--- app/components/TotalDeposit.tsx
+++ app/components/TotalDeposit.tsx
@@ -13,6 +13,7 @@ import { formatNBTC } from "~/lib/denoms";
 
 enum MarketIntegration {
 	TURBOS = "TURBOS",
+	MAGMA = "MAGMA",
 }
 
 interface DepositData {
@@ -35,8 +36,10 @@ const getActionLinks = (integration: MarketIntegration) => {
 	switch (integration) {
 		case MarketIntegration.TURBOS:
 			return action.turbos;
+		case MarketIntegration.MAGMA:
+			return action.magma;
 		default:
-			return "";
+			return null;
 	}
 };
 
@@ -162,5 +165,14 @@ const VAULTS: DApp[] = [
 		apy: 11.71,
 		chain: "SUI",
 		logo: "/assets/ui-icons/market/turbos.svg",
+	},
+	{
+		name: "Magma",
+		type: "DEX",
+		integration: MarketIntegration.MAGMA,
+		labels: ["Farming"],
+		apy: 11.71,
+		chain: "SUI",
+		logo: "/assets/ui-icons/market/magma.svg",
 	},
 ];
