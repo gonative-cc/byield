865e4baa55c950e2442559f62676730ccb5d7e45
rmeena840 <rmeena840@gmail.com>
2025-08-15 21:27:59 +0530

    feedbacks
    Signed-off-by: rmeena840 <rmeena840@gmail.com>

--- app/components/Partners.tsx
+++ app/components/Partners.tsx
@@ -1,7 +1,3 @@
 export function Partners() {
-	return (
-		<div className="w-full max-w-6xl mx-auto px-4">
-			<img src="/assets/auction/partner/partners.png" alt="partners" />
-		</div>
-	);
+	return <img src="/assets/auction/partner/partners.png" alt="partners" />;
 }
