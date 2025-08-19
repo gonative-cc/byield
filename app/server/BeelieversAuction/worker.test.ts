c577831e198ae3e23fb7fbc2e7c268c3b205fb74
Robert Zaremba <robert@zaremba.ch>
2025-08-19 19:38:28 +0200

    update worker.test
    Signed-off-by: Robert Zaremba <robert@zaremba.ch>

--- app/server/BeelieversAuction/worker.test.ts
+++ app/server/BeelieversAuction/worker.test.ts
@@ -13,12 +13,9 @@ describe("worker", () => {
 	beforeEach(async () => {
 		worker = new Miniflare({
 			modules: true,
-			script: `export default { fetch(request, env) { return new Response("OK"); } }`,
-			// bindings: {
-			// 	FOO: "Hello Bindings",
-			// },
+			script: "",
 			kvNamespaces: ["KV2"],
-			// d1Databases: ["DB"],
+			d1Databases: ["DB"],
 			kvPersist: false,
 			d1Persist: false,
 			cachePersist: false,
@@ -30,34 +27,30 @@ describe("worker", () => {
 	});
 
 	test("hello world", async () => {
-		// const bindings = await worker.getBindings();
-		// expect(bindings.FOO).toEqual("Hello Bindings");
-
-		let kv = await worker.getKVNamespace("KV2");
+		const bindings = await worker.getBindings();
+		expect(bindings.FOO).toEqual("Hello Bindings");
 
 		// we can use worker.getKVNamespace("KV2") or bindings.KV2
-		//
-		// await bindings.KV2.put("key", "value");
-		// expect(await bindings.KV.get("key")).toEqual("value");
-		// assert.strictEqual(
-		// 	await (await worker.dispatchFetch("http://example.com")).text(),
-		// 	"Hello World",
-		// );
+		// let kv = await worker.getKVNamespace("KV2");
+		let kv = bindings.KV2 as KVNamespace;
+		await kv.put("key", "value");
+		expect(await kv.get("key")).toEqual("value");
 	});
 
 	test.skip("test-db", async () => {
-		/*
-		const bindings = worker.getBindings();
-		const db = bindings.DB as D1Database;
-		// const db = await worker.getD1Database("DB")
-		 await db.exec("DROP TABLE IF EXISTS bids;");
-		 await db.exec("CREATE TABLE bids (bidder TEXT PRIMARY KEY);");
-		 const stmt = db.prepare("INSERT INTO bids (bidder) VALUES (?)");
-		 const result = await stmt.bind("Robert").run();
-		 expect(result.success).toBe(true);
+		// const bindings = worker.getBindings();
+		// const db = bindings.DB as D1Database;
+		const db = await worker.getD1Database("DB");
+		await db.exec("DROP TABLE IF EXISTS bids;");
+		await db.exec("CREATE TABLE bids (bidder TEXT PRIMARY KEY);");
+		const stmt = db.prepare("INSERT INTO bids (bidder) VALUES (?)");
+		const result = await stmt.bind("Robert").run();
+		expect(result.success).toBe(true);
 
-		 const b = await db.prepare("SELECT * FROM bids WHERE bidder = ?").bind("Robert").first<{bidder: string}>();
-		 expect(b).not.toEqual({bidder: "Robert"});
-		 */
+		const b = await db
+			.prepare("SELECT * FROM bids WHERE bidder = ?")
+			.bind("Robert")
+			.first<{ bidder: string }>();
+		expect(b).not.toEqual({ bidder: "Robert" });
 	});
 });
