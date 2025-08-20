c614d86cb59b03c0258d694c2cc475bfbf8da848
Robert Zaremba <robert@zaremba.ch>
2025-08-20 08:03:19 +0200

    FIX Vitest setup to work with Miniflare
    Signed-off-by: Robert Zaremba <robert@zaremba.ch>

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,16 +4,17 @@ import path from "path";
 export default defineConfig({
 	test: {
 		globals: true, // Enable global test APIs (describe, it, expect, etc.)
-		environment: "jsdom", // Use jsdom for DOM-related tests
-		// setupFiles: "app/vitest.setup.ts",
+		// setting jsdom environment is needed for DOM-related tests. But it breaks test setup -
+		// it throws assertion error when we try to setup Miniflare.
+		// environment: "jsdom",
+		setupFiles: "app/vitest.setup.ts",
 		poolOptions: {
 			workers: {
 				wrangler: { configPath: "./wrangler.jsonc" },
 			},
-
 			// we can add more bindings, see https://developers.cloudflare.com/workers/testing/vitest-integration/write-your-first-test/
 			// miniflare: {
-			// 	d1Databases: { DBT: "TestAuctionDB" },
+			// 	d1Databases: { DBTest: "TestAuctionDB" },
 			// 	d1Persist: false,
 			// },
 		},
