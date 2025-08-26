6e0c77f5b7313b2f3b41a67973d688acd4dfdb5e
Robert Zaremba <robert@zaremba.ch>
2025-08-26 15:00:05 +0200

    feat(raffle): add info about claim
    Signed-off-by: Robert Zaremba <robert@zaremba.ch>

--- app/pages/BeelieversAuction/RaffleStats.tsx
+++ app/pages/BeelieversAuction/RaffleStats.tsx
@@ -8,7 +8,9 @@ interface RaffleStatsProps {
 export function RaffleStats({ totalRaffleInMist }: RaffleStatsProps) {
 	return (
 		<div className="flex flex-col sm:flex-row gap-2 sm:gap-6 w-full max-w-3xl">
-			<StatsCard title={formatSUI(BigInt(totalRaffleInMist)) + " SUI"}>Total Raffle amount</StatsCard>
+			<StatsCard title={formatSUI(BigInt(totalRaffleInMist)) + " nBTC"}>
+				Total Raffle amount. Winners will be able to claim nBTC once the mainnet is live.
+			</StatsCard>
 		</div>
 	);
 }
