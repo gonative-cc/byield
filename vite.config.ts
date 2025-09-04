fda411644ec5b53f27d55d33883534a15b1bc841
Ravindra Kumar Meena <rmeena840@gmail.com>
2025-09-04 14:32:07 +0530

    fix: buffer issue (#333)
    Signed-off-by: rmeena840 <rmeena840@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,31 @@ import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { nodePolyfills } from "vite-plugin-node-polyfills";
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 
 export default defineConfig({
 	plugins: [
+		cloudflare({ viteEnvironment: { name: "ssr" } }),
 		tailwindcss(),
 		reactRouter(),
 		tsconfigPaths(),
-		nodePolyfills({
-			globals: {
-				Buffer: true,
-			},
-		}),
 		cloudflare(),
 	],
 	build: {
 		minify: true,
+	},
+	optimizeDeps: {
+		esbuildOptions: {
+			// Node.js global to browser globalThis
+			define: {
+				global: "globalThis",
+			},
+			// Enable esbuild polyfill plugins
+			plugins: [
+				NodeGlobalsPolyfillPlugin({
+					buffer: true,
+				}),
+			],
+		},
 	},
 });
