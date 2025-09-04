d4613ea93fa188fa91c75b310b2d30efd828d6a3
Rayane Charif <rayane.charif@gonative.cc>
2025-09-04 12:05:20 +0200

    Fix: Same as Master branch regarding the cloudfare issue now resolved
    Signed-off-by: Rayane Charif <rayane.charif@gonative.cc>

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,33 @@
 import { reactRouter } from "@react-router/dev/vite";
-import { cloudflareDevProxy } from "@react-router/dev/vite/cloudflare";
+import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 
 export default defineConfig({
-	plugins: [tailwindcss(), cloudflareDevProxy(), reactRouter(), tsconfigPaths()],
+	plugins: [
+		cloudflare({ viteEnvironment: { name: "ssr" } }),
+		tailwindcss(),
+		reactRouter(),
+		tsconfigPaths(),
+		cloudflare(),
+	],
 	build: {
 		minify: true,
+	},
+	optimizeDeps: {
+		esbuildOptions: {
+			// Node.js global to browser globalThis
+			define: {
+				global: "globalThis",
+			},
+			// Enable esbuild polyfill plugins
+			plugins: [
+				NodeGlobalsPolyfillPlugin({
+					buffer: true,
+				}),
+			],
+		},
 	},
 });
