d4a9605b9413dcdc4005d52a9b5a59fe753853c8
Rayane Charif <rayane.charif@gonative.cc>
2025-09-04 12:05:20 +0200

    Fix: Same as Master branch regarding the cloudfare issue now resolved
    Signed-off-by: Rayane Charif <rayane.charif@gonative.cc>

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import { reactRouter } from "@react-router/dev/vite";
-import { cloudflareDevProxy } from "@react-router/dev/vite/cloudflare";
+import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-	plugins: [tailwindcss(), cloudflareDevProxy(), reactRouter(), tsconfigPaths()],
+	plugins: [
+		cloudflare({ viteEnvironment: { name: "ssr" } }),
+		tailwindcss(),
+		reactRouter(),
+		tsconfigPaths(),
+	],
 	build: {
 		minify: true,
 	},
