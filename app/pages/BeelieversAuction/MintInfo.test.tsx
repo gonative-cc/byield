896f4d1325e051dfcbc77e3e5cee0fffa5e9ce47
Robert Zaremba <robert@zaremba.ch>
2025-09-05 00:28:20 +0200

    refactor(nft mint): reorganize components, fix NFT display
    Signed-off-by: Robert Zaremba <robert@zaremba.ch>

--- app/pages/BeelieversAuction/MintInfo.test.tsx
+++ app/pages/BeelieversAuction/MintInfo.test.tsx
@@ -1,12 +1,12 @@
 import { test, expect } from "vitest";
-import { formatSuiErr } from "./MintInfo";
+import { formatSuiMintErr } from "./MintInfo";
 
 test("formatSuiErr", () => {
 	let errMsg = `Error minting: TRPCClientError: Dry run failed, could not automatically determine a budget: MoveAbort(MoveLocation { module: ModuleId { address: 3064d43ee6cc4d703d4c10089786f0ae805b24d2d031326520131d78667ffc2c, name: Identifier("mint") }, function: 27, instruction: 73, function_name: Some("mint") }, 4) in command 1`;
-	let txErr = formatSuiErr(errMsg);
+	let txErr = formatSuiMintErr(errMsg);
 	expect(txErr).toBe('Tx aborted, function: Some("mint") reason: "user already minted"');
 
 	errMsg = `Error minting: TRPCClientError: User rejected the request`;
-	txErr = formatSuiErr(errMsg);
+	txErr = formatSuiMintErr(errMsg);
 	expect(txErr).toEqual("User rejected the request");
 });
