8897318dd0f35078afd228f945a249af2c352db8
Robert Zaremba <robert@zaremba.ch>
2025-09-04 20:06:24 +0200

    feat(mint): handle 'user rejected' tx error
    Signed-off-by: Robert Zaremba <robert@zaremba.ch>

--- app/pages/BeelieversAuction/MintInfo.test.tsx
+++ app/pages/BeelieversAuction/MintInfo.test.tsx
@@ -2,7 +2,11 @@ import { test, expect } from "vitest";
 import { formatSuiErr } from "./MintInfo";
 
 test("formatSuiErr", () => {
-	const errMsg = `Error minting: TRPCClientError: Dry run failed, could not automatically determine a budget: MoveAbort(MoveLocation { module: ModuleId { address: 3064d43ee6cc4d703d4c10089786f0ae805b24d2d031326520131d78667ffc2c, name: Identifier("mint") }, function: 27, instruction: 73, function_name: Some("mint") }, 4) in command 1`;
-	const moveErr = formatSuiErr(errMsg);
-	expect(moveErr).toBe('Tx aborted, function: Some("mint") reason: "user already minted"');
+	let errMsg = `Error minting: TRPCClientError: Dry run failed, could not automatically determine a budget: MoveAbort(MoveLocation { module: ModuleId { address: 3064d43ee6cc4d703d4c10089786f0ae805b24d2d031326520131d78667ffc2c, name: Identifier("mint") }, function: 27, instruction: 73, function_name: Some("mint") }, 4) in command 1`;
+	let txErr = formatSuiErr(errMsg);
+	expect(txErr).toBe('Tx aborted, function: Some("mint") reason: "user already minted"');
+
+	errMsg = `Error minting: TRPCClientError: User rejected the request`;
+	txErr = formatSuiErr(errMsg);
+	expect(txErr).toEqual("User rejected the request");
 });
