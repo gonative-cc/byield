c3840c9e7fec0308c08227da4de4ac4cc5b10fa5
rmeena840 <rmeena840@gmail.com>
2025-09-05 20:38:30 +0530

    feat: copy address or tx in mint nBTC
    Signed-off-by: rmeena840 <rmeena840@gmail.com>

---
+++ app/components/ui/CopyButton.tsx
@@ -0,0 +1,30 @@
+import { Check, Copy } from "lucide-react";
+import { useState } from "react";
+
+interface CopyButtonProps {
+	text: string;
+}
+
+export function CopyButton({ text }: CopyButtonProps) {
+	const [copied, setCopied] = useState(false);
+
+	const handleCopy = async () => {
+		try {
+			await navigator.clipboard.writeText(text);
+			setCopied(true);
+			setTimeout(() => setCopied(false), 2000);
+		} catch (err) {
+			console.error("Failed to copy text: ", err);
+		}
+	};
+
+	return (
+		<button onClick={handleCopy} className={"hover:bg-primary/10"}>
+			{copied ? (
+				<Check className="text-green-500" size={16} />
+			) : (
+				<Copy size={16} className="text-primary" />
+			)}
+		</button>
+	);
+}
