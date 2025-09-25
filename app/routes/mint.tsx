81c4018112983decc0a5bd6a78112f3c8cf2a35f
rmeena840 <rmeena840@gmail.com>
2025-09-25 14:04:01 +0530

    feat: FAQ in mint nbtc
    Signed-off-by: rmeena840 <rmeena840@gmail.com>

--- app/routes/mint.tsx
+++ app/routes/mint.tsx
@@ -5,6 +5,83 @@ import { Collapse } from "~/components/ui/collapse";
 import { useNbtcTxs } from "~/hooks/useNbtcTransactions";
 import { RefreshCw } from "lucide-react";
 import { BlockInfoCard } from "~/components/ui/BlockInfoCard";
+import { FAQ } from "~/components/FAQ";
+
+const FAQS = [
+	{
+		id: "faq-1",
+		question: "What is the main purpose of Testnet v2?",
+		answer: `The primary focus is to test the end-to-end flow of minting nBTC on the Sui testnet using BTC from our custom Bitcoin devnet. We are focused on security, functionality, and bug hunting.`,
+	},
+	{
+		id: "faq-2",
+		question: `What is this "custom Bitcoin network"?`,
+		answer: "It's our own devnet where we have full control. It produces a new block every two minutes and has no re-orgs, providing a stable environment for testing the core logic.",
+	},
+	{
+		id: "faq-3",
+		question: "What are the key new features in V2?",
+		answer: "The single most important feature is minting nBTC directly from BTC using our SPV Proofs.",
+	},
+	{
+		id: "faq-4",
+		question: "Will the old way of swapping SUI for nBTC still work?",
+		answer: "Yes, both options will be available. You can swap SUI for nBTC, and you can mint nBTC from BTC..",
+	},
+	{
+		id: "faq-5",
+		question: "Why do I need to get BTC from you? Is there a faucet?",
+		answer: (
+			<span className="text-muted-foreground">
+				To ensure a controlled testing environment, we are distributing devnet BTC manually. We do not
+				have a public faucet for this phase. Please request funds in our{" "}
+				<a
+					href="https://forms.gle/nxSr94kN4BiVpJpx6"
+					target="_blank"
+					rel="noreferrer"
+					className="link link-primary"
+				>
+					form.
+				</a>
+			</span>
+		),
+	},
+	{
+		id: "faq-6",
+		question: "Which Bitcoin wallet do I need?",
+		answer: "You must use the Xverse wallet, as it allows for connection to our custom devnet network.",
+	},
+	{
+		id: "faq-7",
+		question: "Which Sui wallet do I need?",
+		answer: "Any Sui wallet that we currently support will work. You can also insert a Sui Address manually, without having to connect your Sui Wallet.",
+	},
+	{
+		id: "faq-8",
+		question: "Is there a minimum or maximum amount of nBTC I can mint?",
+		answer: "No, there are no limits on the amount you can mint in a single transaction.",
+	},
+	{
+		id: "faq-9",
+		question: "Can I sell or withdraw my nBTC back to BTC?",
+		answer: "Not yet. For now, it’s only one-way for now (Bitcoin -> Sui). The ability to withdraw nBTC back to BTC will be a future update.",
+	},
+	{
+		id: "faq-10",
+		question: "How do I report bugs?",
+		answer: "Open a ticket in #testnet-v2-feedback using the template; include addresses, txid, steps to reproduce, timestamps, and screenshots.",
+	},
+	{
+		id: "faq-11",
+		question: "Is this Testnet V2 Incentivized in any way?",
+		answer: "No. This is a targeted technical preview, it’s limited to users and partners that want to help us test it, understand the flow and provide feedback!",
+	},
+	{
+		id: "faq-12",
+		question: "What happens after v2?",
+		answer: "We’ll publish a findings report, patch critical/high issues, and work towards the Testnet V3, which will have more functionality and focus on user-testing!",
+	},
+];
 
 export default function Mint() {
 	const { txs: transactions, isLoading, error, refetch, addPendingTx } = useNbtcTxs();
@@ -59,6 +136,9 @@ export default function Mint() {
 				)}
 
 				<MintBTCTable data={transactions} isLoading={isLoading} />
+			</div>
+			<div className="flex justify-center">
+				<FAQ faqs={FAQS} />
 			</div>
 		</div>
 	);
