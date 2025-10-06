bd1c1267497a2e54d0f6f05ed5999906d0a52379
Robert Zaremba <robert@zaremba.ch>
2025-10-06 12:42:56 +0200

    feat: server error handling (#524)
    * feat: server error handling
    * use error log
    * rename postNBTCTx to postNbtcTx
    
    Signed-off-by: Robert Zaremba <robert@zaremba.ch>

--- app/server/http-resp.ts
+++ app/server/http-resp.ts
@@ -1,3 +1,5 @@
+import { logError, logHttpError } from "~/lib/log";
+
 export function badRequest(msg: string = "Bad Request"): Response {
 	return new Response(msg, { status: 400 });
 }
@@ -14,11 +16,30 @@ export function notImplemented(): Response {
 	return new Response("Not Implemented", { status: 501 });
 }
 
-export function serverError(msg: string = "Server Error"): Response {
+// creates response, with content-type header set to text/plain
+export function textOK(o: string | null): Response {
+	return new Response(o);
+}
+
+// logs and handles server error
+// * method: server method ID, should be in the following format: <server_name>:<method_name>
+export function serverError(
+	method: string,
+	error: unknown,
+	msg: string = "Server Error",
+): Response {
+	logError({ method, msg }, error);
 	return new Response(msg, { status: 500 });
 }
 
 // creates response, with content-type header set to text/plain
-export function textOK(o: string | null): Response {
-	return new Response(o);
+// * method: server method ID, should be in the following format: <server_name>:<method_name>
+export async function handleNonSuccessResp(
+	method: string,
+	msg: string,
+	r: Response,
+): Promise<Response> {
+	const ctx = { method, msg };
+	const body = await logHttpError(ctx, r);
+	return new Response(msg + ".\n" + body, { status: r.status });
 }
