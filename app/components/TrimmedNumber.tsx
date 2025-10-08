a8cd0bc143762415293f3142849c4ef731e0ef2d
rmeena840 <rmeena840@gmail.com>
2025-10-08 16:06:41 +0530

    PR comments
    Signed-off-by: rmeena840 <rmeena840@gmail.com>

--- app/components/TrimmedNumber.tsx
+++ app/components/TrimmedNumber.tsx
@@ -3,7 +3,15 @@ import { Tooltip } from "./ui/tooltip";
 import { type NumericFormatProps } from "react-number-format";
 
 export function TrimmedNumber(props: NumericFormatProps) {
-	if (!props.value) return null;
+	if (
+		props.value === null ||
+		props.value === undefined ||
+		props.value === "" ||
+		((typeof props.value === "bigint" || typeof props.value === "number") && props.value !== 0) ||
+		isNaN(Number(props.value))
+	) {
+		return null;
+	}
 
 	const numValue = Number(props.value);
 	const decimalScale = numValue >= 1 ? 3 : undefined;
