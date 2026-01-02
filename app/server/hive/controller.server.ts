446d50c6fa33d41a106dbf10c3db9b79d0671c7d
rmeena840 <rmeena840@gmail.com>
2026-01-02 19:46:45 +0530

    feat: use graphql query to fetch the total locked deposit
    Signed-off-by: rmeena840 <rmeena840@gmail.com>

--- app/server/hive/controller.server.ts
+++ app/server/hive/controller.server.ts
@@ -1,14 +1,18 @@
+import type { GraphQLResponse } from "../common/types";
 import { badRequest, notFound, serverError } from "../http-resp";
-import type { QueryAllUserDataResp, QueryUserDataResp, Req } from "./jsonrpc";
+import type { QueryUserDataResp, QueryUserTotalDepositDataResp, Req } from "./jsonrpc";
 import { logError } from "~/lib/log";
+import type { UserUSDCTotalDeposit } from "./types";
 
 const URL = "https://rd-api-staging.tbook.com/sbt-data/gonative";
 
 export class HiveController {
 	tbookAuthToken: string = "";
+	suiGraphQLURl: string | null = null;
 
-	constructor(_tbookAuthToken: string) {
+	constructor(_tbookAuthToken: string, _graphqlURl: string) {
 		this.tbookAuthToken = _tbookAuthToken;
+		this.suiGraphQLURl = _graphqlURl || null;
 	}
 
 	private async fetchData<T>(url: string, options?: RequestInit): Promise<T | Response> {
@@ -39,9 +43,67 @@ export class HiveController {
 		return await this.fetchData<QueryUserDataResp>(url);
 	}
 
-	async queryAllUserData(): Promise<QueryAllUserDataResp | Response> {
-		const url = `${URL}/all-users`;
-		return await this.fetchData<QueryAllUserDataResp>(url);
+	async queryTotalDeposit(
+		contractId: string,
+		suiAddr: string,
+	): Promise<QueryUserTotalDepositDataResp | Response> {
+		try {
+			if (!this.suiGraphQLURl) throw badRequest();
+			const endpoint = this.suiGraphQLURl;
+			const query = `
+				query UserUSDCTotalDeposit($contractId: String!, $suiAddr: SuiAddress!) {
+					events(
+						filter: {
+						module: $contractId
+						sender: $suiAddr
+						}
+						last: 1
+					) {
+						nodes {
+						timestamp
+						sender {
+							address
+						}
+						contents {
+							json
+						}
+						transaction {
+							digest
+							effects {
+							status
+							}
+						}
+						}
+					}
+				}
+			`;
+
+			const res = await fetch(endpoint, {
+				method: "POST",
+				body: JSON.stringify({
+					query: query,
+					variables: {
+						contractId,
+						suiAddr,
+					},
+				}),
+			});
+			if (!res.ok) throw new Error(res.statusText);
+			const { data, errors } = await res.json<GraphQLResponse<UserUSDCTotalDeposit>>();
+			if (errors?.length) {
+				throw new Error(errors.map((e) => e.message).join(", "));
+			}
+			console.log(data.events.nodes);
+			return {
+				code: res.status,
+				data: data.events.nodes?.[0]?.contents?.json?.total_amount || "0",
+				isError: false,
+				message: "Success",
+			};
+		} catch (err) {
+			logError({ msg: "Error fetching total deposit", method: "queryTotalDeposit" }, err);
+			return serverError("queryTotalDeposit", err, "Error fetching total deposit");
+		}
 	}
 
 	async handleJsonRPC(r: Request) {
@@ -61,8 +123,8 @@ export class HiveController {
 		switch (reqData.method) {
 			case "queryHiveUserData":
 				return this.queryUserData(reqData.params[0]);
-			case "queryAllUserData":
-				return this.queryAllUserData();
+			case "queryTotalDeposit":
+				return this.queryTotalDeposit(reqData.params[1], reqData.params[2]);
 			default:
 				return notFound("Unknown method");
 		}
