f7520008d1920ecb8584025f3ede48a6278bd783
Robert Zaremba <robert@zaremba.ch>
2026-01-15 10:04:10 +0100

    db(byield): remove cbtc table and add params table
    Signed-off-by: Robert Zaremba <robert@zaremba.ch>

--- db/byield/0001_init.sql
+++ db/byield/0001_init.sql
@@ -1,9 +1,8 @@
 -- Migration number: 0001 	 2025-11-06T17:41:33.505Z
 
-CREATE TABLE IF NOT EXISTS cbtc (
-  network   TEXT NOT NULL,
+-- key/value store for various parameters
+CREATE TABLE IF NOT EXISTS params (
+  setup_id  NUMBER NOT NULL,
   name      TEXT NOT NULL,
-  btc_addr  TEXT NOT NULL,
-  cbtc_pkg  TEXT NOT NULL,
-  cbtc_obj  TEXT NOT NULL,
-  note      TEXT);
+  value     BLOB NOT NULL
+);
